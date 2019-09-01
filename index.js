74618a48132e0927a0a6c0a15633d27ec81b6ed6
Mike Meyer <mfmeyer301@gmail.com>
2019-09-01 17:30:39 +0200

    register

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const authRoute = require('./routes/auth');
 
 const app = express();
 
-// Connect to DB
-mongoose.connect('mongodb connect link', { useNewUrlParser: true }, () =>
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
 	console.log('connected to db')
 );
 
-// Import Routes
-const authRoute = require('./routes/auth');
+app.use(express.json());
 
-// Route Middleware
 app.use('/api/user', authRoute);
 
 app.listen(3000, () => console.log('Server is up and running...'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
-router.post('/register', (req, res) => {
-	res.send('Register');
+const User = require('../models/User');
+const { registerValidation } = require('../validation');
+
+router.post('/register', async (req, res) => {
+	const { error } = registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const emailExist = await User.findOne({ email: req.body.email });
+	if (emailExist) return res.status(400).send('Email already exists');
+
+	const salt = await bcrypt.gentSalt(10);
+	const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+	const user = new User({
+		name: req.body.name,
+		email: req.body.email,
+		password: hashPassword
+	});
+	try {
+		const saveUser = await user.save();
+		res.send({ user: user._id });
+	} catch (err) {
+		res.status(400).send(err);
+	}
 });
 
 router.post('/login', (req, res) => {
